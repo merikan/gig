febab07d7e91214c50e4bbc20fbe093d1164d3e1
Peter Merikan <peter@merikan.com>
2026-03-28 09:28:34 +0100

    test: cover git_config's error-propagation branches

--- tests/config_root_dir.rs
+++ tests/config_root_dir.rs
@@ -45,3 +45,34 @@ fn viewing_unset_root_dir_errors_with_the_command_to_run() {
         .failure()
         .stderr(contains("git-get config root-dir <path>"));
 }
+
+#[test]
+fn viewing_root_dir_propagates_a_real_git_config_error() {
+    let harness = GitGetTest::new();
+
+    harness
+        .cmd()
+        .env("GIT_GET_STUB_EXIT_CODE", "2")
+        .env(
+            "GIT_GET_STUB_STDERR",
+            "fatal: bad config line 3 in file .gitconfig",
+        )
+        .args(["config", "root-dir"])
+        .assert()
+        .failure()
+        .stderr(contains("fatal: bad config line 3 in file .gitconfig"));
+}
+
+#[test]
+fn setting_root_dir_propagates_a_git_config_error() {
+    let harness = GitGetTest::new();
+
+    harness
+        .cmd()
+        .env("GIT_GET_STUB_EXIT_CODE", "1")
+        .env("GIT_GET_STUB_STDERR", "fatal: could not lock config file")
+        .args(["config", "root-dir", "/tmp/somewhere"])
+        .assert()
+        .failure()
+        .stderr(contains("fatal: could not lock config file"));
+}
